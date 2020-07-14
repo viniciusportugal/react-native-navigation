5469e5319bd6aa4d107f5f04944a5f9ef6bcecc3
viniciusportugal <vini.aport@gmail.com>
2020-07-14 19:12:06 -0300

    adding icons to tab bar

--- src/navegacao/index.js
+++ src/navegacao/index.js
@@ -3,15 +3,15 @@ import { Text, SafeAreaView } from 'react-native'
 import {NavigationContainer} from'@react-navigation/native'
 
 // import Stack from './Stack'
-// import Tab from './Tab'
-import Drawer from './Drawer'
+import Tab from './Tab'
+// import Drawer from './Drawer'
 
 export default props => (
     <SafeAreaView style={{flex: 1}}>
         <NavigationContainer>
             {/* <Stack /> */}
-            {/* <Tab /> */}
-            <Drawer />
+            <Tab />
+            {/* <Drawer /> */}
         </NavigationContainer>
     </SafeAreaView>
 )
